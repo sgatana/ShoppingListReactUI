664af22eb84c7d529797ec787c8d3b4af4ceeb94
gatana <sgatana@gmail.com>
2018-01-17 15:37:20 +0300

    [Chore] Change default styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './components/accounts/login';
+import Register from './components/accounts/register';
+import Header from './components/header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import registerServiceWorker from './registerServiceWorker';
+import DashBoard from './components/dashboard';
+import NotFound from './components/notFound';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.baseURL='http://127.0.0.1:5000/v1/';
+
+
+ReactDOM.render(
+    <div className="App">
+        <Header />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route  path="/register" component={Register} />
+                <Route  path="/dashboard" component={DashBoard} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    </div>
+    ,
+    document.getElementById('root')
+);
 registerServiceWorker();
