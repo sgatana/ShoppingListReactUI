cbfe38656f156969dfceaa71898786aac60d665f
gatana <sgatana@gmail.com>
2018-01-23 11:30:49 +0300

    [Chore] Add addItem and addList modals

---
+++ src/components/modals/addItem.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class AddItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shoppingList:[],
+            name: '',
+            price: '',
+            quantity: '',
+            unit: ''
+        }
+        this.onAddItem = this.onAddItem.bind(this);
+        
+    }
+    onAddItem = (list_id) => {
+        let itemDetails = new FormData();
+        itemDetails.set("name", this.state.name);
+        itemDetails.set("price", this.state.price);
+        itemDetails.set("quantity", this.state.quantity);
+        itemDetails.set("unit", this.state.unit);
+        this.setState({shoppingList:list_id});
+        
+        axios.post(`/Shoppinglist/${list_id}/Items`, itemDetails,
+    {
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+            'Authorization': localStorage.getItem("token")
+        }
+    })
+    .then(response => {
+        console.log(response.data);
+        window.location.reload();
+    })
+    .catch(error => {
+        console.log(error.response);
+    })
+    }
+
+    onInputChange = (e) => {
+        const itemName = e.target.name;
+        const itemValue = e.target.value;
+        const itemData = {};
+        itemData[itemName] = itemValue;
+        this.setState(itemData);
+    }
+    render() {
+        return (
+            <div className="modal fade" id="add_item" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 className="modal-title" id="add_list">Add Item</h4>
+                        </div>
+                        <form method="post" >
+                            <div className="modal-body">
+
+                                <div className="form-group">
+                                    <label htmlFor="name" className="control-label">Name:</label>
+                                    <input type="text" name='name' required onChange={this.onInputChange} className="form-control" placeholder="Enter shopping item name" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="recipient-name" className="control-label">Price:</label>
+                                    <input type="text" name='price' required onChange={this.onInputChange} className="form-control" placeholder="Enter price" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="recipient-name" className="control-label">Quantity:</label>
+                                    <input type="text" name='quantity' required onChange={this.onInputChange} className="form-control" placeholder="Enter quantity" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="recipient-name" className="control-label">Unit of Measure:</label>
+                                    <input type="text" name='unit' required onChange={this.onInputChange} className="form-control" placeholder="Enter unit of measure e.g kgs" />
+                                </div>
+
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                                <button type="reset" className="btn btn-warning">Cancel</button>
+                                <button type="button" onClick={() =>{this.onAddItem(this.props.listId)}} id="#submit" className="btn btn-primary">Add Item</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+export default AddItem
---
+++ src/components/modals/addList.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+export default class AddList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name:'',
+            description:'',
+            isModalOpen: false
+        }
+        // bind the method to super
+        this.onAddShoppingList = this.onAddShoppingList.bind(this);
+    }
+    
+
+    onAddShoppingList = (e)=> {
+        e.preventDefault();
+        let listData = new FormData();
+        listData.set('name', this.state.name);
+        listData.set('description', this.state.description);
+        axios.post('/Shoppinglist', listData, {
+            headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+            'Authorization':localStorage.getItem("token")
+            }
+        })
+        .then(response => {
+           
+            window.location.reload()
+            
+        })
+        .catch(error => {
+            console.log(error.response)
+        })
+    }
+    onShoppingListInputs =(e) =>{
+        const listName = e.target.name;
+        const listValue = e.target.value;
+        let listDetails = {}
+        listDetails[listName] = listValue;
+        this.setState(listDetails)
+    }
+
+    render() {
+        return (
+            <div className="modal fade" id="add_list" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 className="modal-title" id="add_list">Add Shopping list</h4>
+                        </div>
+                        <form method="post" onSubmit={this.onAddShoppingList}>
+                        <div className="modal-body">
+                            
+                                <div className="form-group">
+                                    <label htmlFor="name" className="control-label">Name:</label>
+                                    <input type="text" name='name' required onChange={this.onShoppingListInputs} className="form-control" placeholder="Enter shopping list name" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="recipient-name" className="control-label">Description:</label>
+                                    <textarea type="text" name='description' onChange={this.onShoppingListInputs} className="form-control" placeholder="Enter the description"></textarea>
+                                </div>
+                            
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                            <button type="reset" className="btn btn-warning">Cancel</button>
+                            <button type="submit" id="#submit" data-target="" className="btn btn-primary">Add Item</button>
+                        </div>
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+        );
+    }
+}
