0d5160d1c160392aa48afca97d4f10e49a9e4e6f
gatana <sgatana@gmail.com>
2018-01-25 00:13:51 +0300

    [Feature] add delete item functionality

--- src/components/items.js
+++ src/components/items.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import Header from './header';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
+import UpdateItem from './modals/updateItem';
 
 
 class Items extends Component {
     constructor(props){
         super(props);
         this.state = {
-            items: []
+            items: [],
+            listId: ''
         }
+    }
+    
+    onItemDelete = (itemId) => {
+        axios.delete(`/Shoppinglist/${this.props.match.params.id}/Items/${itemId}`, {
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded',
+                'Authorization': localStorage.getItem("token")
+            }
+        })
+        .then(response => {
+            console.log(response.data.message)
+            toast.success(response.data.message)
+            this.setState({items: []})
+            // this.getItems(this.props.match.params.id);
+        })
+        .catch(error => {
+            console.log(error.response)
+        })
+    }
+    onItemEdit = () => {
+
     }
     onBackToDashboard = () => {
         this.props.history.push('/dashboard');
@@ -26,12 +49,20 @@ class Items extends Component {
             this.setState({items: response.data.shoppinglist_items})
         })
         .catch(error => {
+            if (error.response.data.error === "your token is invalid, please log in ") {
+                window.localStorage.clear('token');
+                window.location.reload();
+                this.props.history.push('/');
+            }
             console.log(error.response)
             toast.error(error.response.data.error)
 
         })
     }
-    componentDidMount(){
+    componentWillMount(){
+        if (!window.localStorage.getItem('token')) {
+            this.props.history.push('/')
+        }
         this.getItems(this.props.match.params.id);
     }
     render(){ 
@@ -43,8 +74,6 @@ class Items extends Component {
                     <h3>
                         ShoppingList name 
                         <button className="pull-right btn btn-success fa fa-long-arrow-left" onClick={this.onBackToDashboard}>Back To ShoppingList 
-                        {/* <button className="btn btn-lg btn-success"><i className="fa fa-plus" />Add Item</button> */}
-                       
                         </button>
                     </h3>
                 </div>
@@ -64,12 +93,14 @@ class Items extends Component {
                                 this.state.items.map((item) => {
                                     return (
                                         <tr key={item.id}>
+                                            <UpdateItem item={item} />
                                             <td>{item.name}</td>
                                             <td>{item.price}</td>
                                             <td>{item.quantity}</td>
                                             <td>{item.unit}</td>
                                             <td>
-                                                <i className="fa fa-edit " /> |  <i className="fa fa-trash text-danger" />
+                                                <i className="fa fa-edit" data-target={"#update_item" + item.id } data-toggle="modal" /> | 
+                                                <i className="fa fa-trash text-danger" onClick={() => { this.onItemDelete(item.id)}} />
                                             </td>
                                         </tr>
                                     )
