06bfe1a309dcd3fb6a1c33502c682a7dc4dd6bc6
gatana <sgatana@gmail.com>
2018-01-29 15:56:48 +0300

    [Chore] add tests for register component

---
+++ src/__tests__/Register.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow, mount, render} from 'enzyme';
+import Register from '../components/accounts/register';
+
+
+describe('<Register /> component', () => {
+    const wrapper = shallow(<Register />)
+    it('should render without throwing an error', () => {
+        expect(wrapper.find("div")).toHaveLength(8)
+    })
+    it('should render form correctly', () => {
+        // check register component contain one <from/>
+        const RegisterForm = wrapper.find("form");
+        expect(RegisterForm).toHaveLength(1);
+    });
+    it('should render h1', () => {
+        //check that H1 is being rendered
+        expect(wrapper.find('h1').text()).toEqual('User Registration')
+    });
+    it('should render form inputs', () => {
+        expect(wrapper.find('#username').length).toEqual(1);
+        expect(wrapper.find('#password').length).toEqual(1);
+        expect(wrapper.find('#email').length).toEqual(1);
+        expect(wrapper.find('#confirm').length).toEqual(1);
+    });
+    it('input should respond to change event and change the state', () => {
+        wrapper.find('#email').simulate('change', {target : {name:'email', value: 'admin@gmail.com'}});
+        expect(wrapper.state('email')).toEqual('admin@gmail.com')
+    });
+});
+
+
